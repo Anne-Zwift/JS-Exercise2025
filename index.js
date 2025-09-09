b3b92c92461a4bdd8dbcb63cd89b1eb5279aacde
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2025-09-09 10:48:08 +0200

    implemented Vite to the project

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 // index.js
-import { add } from './utils.js';
+/*import { add } from './utils.js';
 
 function start() {
     const sum = add(5, 10);
     console.log(`The sum is: ${sum}`);
 }
 
-start();
+start();*/
+
+//Exercise 2.1 Event Loop
+//Micro-task runs before macro-task
+console.log('A: Script start');//1
+
+fetch('https://v2.api.noroff.dev/old-games/1').then(function () {
+  console.log('B: Promise resolved');//micro, 3
+});
+
+setTimeout(function () {
+  console.log('C: setTimeout finished');//macro, 4
+}, 0);
+
+console.log('D: Script end');//2
+
